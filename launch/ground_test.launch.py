848c8eb256e42a17bf1ff0ea54b199390864de3e
Alfred Thoft <alfchr21@student.aau.dk>
2024-11-20 09:35:04 +0100

    added random mover config to the sdf, and added support for multiple move groupes

---
+++ launch/ground_test.launch.py
@@ -0,0 +1,114 @@
+
+import os
+
+from ament_index_python.packages import get_package_share_directory, re
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.descriptions import executable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+import xml.etree.ElementTree as xmlET
+
+import random 
+import numpy as np
+
+
+def spawn_multiple(fname, base_name, n_units):
+    tree = xmlET.parse(fname)
+    root = tree.getroot()
+    model = root.find(".//model")
+    
+    spawners = []
+
+    for n in range(n_units):
+        model.attrib['name'] = f"{base_name}_{n}"
+        tree.write(f"/tmp/{base_name}_{n}n.sdf")
+
+        spawner = Node(package='ros_gz_sim',
+                       executable='create',
+                       name=f"spawn_{base_name}_{n}",
+                       arguments=['-name', f"{base_name}_{n}", '-file', f"/tmp/{base_name}_{n}n.sdf"])
+        spawners.append(spawner)
+
+    return spawners
+    
+
+
+    
+def generate_launch_description():
+    lidar_sim_dir = get_package_share_directory("lidar_sim")
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ), 
+        # set it to use the empty world specified in the pgk 
+        launch_arguments={'gz_args': os.path.join(lidar_sim_dir, 'models', 'empty_world.sdf') + ' -v 4' ' --headless-rendering' + ' -r'}.items(),)
+
+    lidar_model = "ouster_os1"
+    lidar_sdf_path = os.path.join(lidar_sim_dir,"models",f"{lidar_model}.sdf")
+    
+
+    spawn_lidar = Node(package='ros_gz_sim',
+                               executable='create',
+                               name='spawn_lidar',
+                               arguments=['-name', lidar_model, '-file', lidar_sdf_path],
+                       output='screen')
+
+    fendt_sdf_path = os.path.join(lidar_sim_dir,"models","fendt.sdf")
+
+
+    spawn_fendt = Node(package='ros_gz_sim',                       
+                               executable='create',
+                               name='spawn_fendt',
+                               arguments=['-name', "fendt", '-file', fendt_sdf_path],
+                               output='screen')
+
+
+    target_spawners = []
+
+    targets_sdf_paths = os.path.join(lidar_sim_dir, "models", "ground")
+
+    for target_sdf_path in os.listdir(targets_sdf_paths):
+        target_spawners = target_spawners + spawn_multiple(os.path.join(targets_sdf_paths,target_sdf_path), target_sdf_path.split(".")[0], 20)
+    
+
+
+    lidar_bridge = Node(package='ros_gz_bridge',
+                        executable = 'parameter_bridge',
+                        name='scan_bridge',
+                        arguments=["/scan/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked"],
+                        output = 'screen')
+
+
+
+
+    lidar_static_transform = Node(package='tf2_ros',
+                                  executable = 'static_transform_publisher',
+                                  name='lidar_tf_pub',
+                                  arguments= [
+                                  '0',
+                                  '0',
+                                  '1',
+                                  '0',
+                                  '0',
+                                  '0',                 
+                                  "world",
+                                  lidar_model+"/lidar_link/gpu_lidar" 
+                                ])
+    return LaunchDescription([gz_sim, 
+                              # spawn_lidar, 
+                              # lidar_bridge, 
+                              lidar_static_transform] + target_spawners)
+
+
+if __name__ == "__main__":
+    lidae_sim_dir = get_package_share_directory("lidar_sim")
+    
+    sdf_path = os.path.join(lidae_sim_dir,"models","fendt.sdf")
+
+    spawn_multiple(sdf_path, "fendt", 3)
--- src/random_mover.cc
+++ src/random_mover.cc
@@ -22,14 +22,27 @@
 #include <ignition/transport/Node.hh>
 #include <ignition/plugin.hh>
 #include <ignition/math.hh>
+#include <unordered_set>
 #include <vector>
 
 namespace lidar_sim
 {
 
 class RandomMoverPrivate{
-  public: std::set<std::string> target_move_group; 
-  public: std::set<std::string> tracker_move_group; 
+public: 
+  // std::set<std::string> target_move_group; 
+  // std::set<std::string> tracker_move_group;
+  // std::set<std::string> ground_move_group;
+  std::string model_name;
+  std::string move_group;
+  std::vector<double> range;
+  std::vector<double> z_range;
+  std::vector<double> roll_range;
+  std::vector<double> pitch_range;
+  std::vector<double> yaw_range;
+  double appearance_p;
+  double min_dist; //minimum distance to others 
+
 };
 
 
@@ -53,20 +66,20 @@ gz::math::Pose3d RandomMover::generate_random_tracker_pose(double min_z, double 
 } 
 
 
-gz::math::Pose3d generate_random_pose(double min_dist, double max_dist){
+gz::math::Pose3d RandomMover::generate_random_pose(){
 
-  double distance = gz::math::Rand::DblUniform(min_dist, max_dist);
+  double distance = gz::math::Rand::DblUniform(data_ptr->range[0], data_ptr->range[1]);
   double angle = gz::math::Rand::DblUniform(0, 2*M_PI);
 
 
   double x = std::cos(angle)*distance;
   double y = std::sin(angle)*distance;
-  double z = gz::math::Rand::DblUniform(0, 0.1);
+  double z = gz::math::Rand::DblUniform(data_ptr->z_range[0], data_ptr->z_range[1]);
   
   // Generate random orientation values (optional, or keep orientation constant)
-  double roll = gz::math::Rand::DblUniform(0, 0.1 * M_PI);
-  double pitch = gz::math::Rand::DblUniform(0, 0.1 * M_PI);
-  double yaw = gz::math::Rand::DblUniform(0, 2 * M_PI);
+  double roll = gz::math::Rand::DblUniform(data_ptr->roll_range[0], data_ptr->roll_range[1]);
+  double pitch = gz::math::Rand::DblUniform(data_ptr->pitch_range[0], data_ptr->pitch_range[1]);
+  double yaw = gz::math::Rand::DblUniform(data_ptr->yaw_range[0], data_ptr->yaw_range[1]);
   return  gz::math::Pose3d(x, y, z, roll, pitch, yaw);
 }
 
@@ -85,54 +98,76 @@ double distance_between_poses(gz::math::Pose3d pose_a, gz::math::Pose3d pose_b){
 }
 
 
-std::vector<gz::math::Pose3d> RandomMover::generate_random_target_poses(double min_dist, double max_dist, int n_poses, double visible_threshold) {
+std::vector<gz::math::Pose3d> RandomMover::generate_random_target_poses() {
   // Generate random position values within the specified range
-  auto& pose_vec = PoseStorage::Instance().get_poses();
+  auto& grouped_pose_vecs = PoseStorage::Instance().get_poses();
 
-  igndbg<<"generating poses for " << n_poses << " targets"<<std::endl;
-  for (int i = 0; i < n_poses; i++){
-    igndbg << i <<std::endl;
-    gz::math::Pose3d random_pose = generate_random_pose(min_dist, max_dist);
-    // radomize if it should be visible
-    if (visible_threshold < gz::math::Rand::DblUniform(0,1)){
-      random_pose.SetZ(-50); 
-      pose_vec.push_back(random_pose);
-      igndbg << "target not visible" <<std::endl;
-    }
-    else{
-      bool not_clear_of_others = true;
-    
-      if (pose_vec.empty()){
-        igndbg << "pose_vec is empty" << std::endl;
-        not_clear_of_others = false;
-      }
-
-      while (not_clear_of_others) {
-      
-        double closest_distance_to_other = 99;
-        igndbg << "pose vec size "<< pose_vec.size() << std::endl;
-        for(const gz::math::Pose3d& pose : pose_vec){
-          double dist = distance_between_poses(pose, random_pose);
-          if (dist < closest_distance_to_other){
-            closest_distance_to_other = dist;
-          }
-        }
-        igndbg<<"closest_distance_to_other "<<closest_distance_to_other<<std::endl;
-        if (closest_distance_to_other < 7){
-          random_pose = generate_random_pose(min_dist, max_dist);
-          igndbg<<"target not clear"<<std::endl;
-          igndbg<<closest_distance_to_other<<std::endl;
-        }
-        else {
-          not_clear_of_others = false;
-          igndbg<< "target clear" <<std::endl;
-        }
-      }  
-      pose_vec.push_back(random_pose);
+  std::vector<gz::math::Pose3d> pose_vec;
+  for (GroupedPoseVec gpv : grouped_pose_vecs){
+    if (gpv.move_group == this->data_ptr->move_group){
+      pose_vec = gpv.poses;
+      igndbg << "using pose vec for move group "<<gpv.move_group<<std::endl;
+    } 
+    else {
+      igndbg << "no pose vec found for move group" <<gpv.move_group<<std::endl;
     }
   }
+  igndbg<<"recived pose vec size "<<pose_vec.size()<<std::endl;
+
+  
+  gz::math::Pose3d random_pose = generate_random_pose();
+  // radomize if it should be visible
+  if (data_ptr->appearance_p < gz::math::Rand::DblUniform(0,1)){
+    random_pose.SetZ(-50); 
+    pose_vec.push_back(random_pose);
+    igndbg << "target not visible" <<std::endl;
+  }
+  else{
+    bool not_clear_of_others = true;
+  
+    if (pose_vec.empty()){
+      igndbg << "pose_vec is empty" << std::endl;
+      not_clear_of_others = false;
+    }
+
+    while (not_clear_of_others) {
+    
+      double closest_distance_to_other = 99;
+      igndbg << "pose vec size "<< pose_vec.size() << std::endl;
+      for(const gz::math::Pose3d& pose : pose_vec){
+        double dist = distance_between_poses(pose, random_pose);
+        if (dist < closest_distance_to_other){
+          closest_distance_to_other = dist;
+        }
+      }
+      igndbg<<"closest_distance_to_other "<<closest_distance_to_other<<std::endl;
+      if (closest_distance_to_other < 7){
+        random_pose = generate_random_pose();
+        igndbg<<"target not clear"<<std::endl;
+        igndbg<<closest_distance_to_other<<std::endl;
+      }
+      else {
+        not_clear_of_others = false;
+        igndbg<< "target clear" <<std::endl;
+      }
+    }
+    
+    int n_move_groups = grouped_pose_vecs.size();
+    
+    for (int i = 0;i < n_move_groups; i++){
+      if (grouped_pose_vecs[i].move_group == this->data_ptr->move_group){
+
+        grouped_pose_vecs[i].poses.push_back(random_pose);
+        igndbg << "adding pose to move group "<<grouped_pose_vecs[i].move_group<<std::endl;
+      }  
+    }
+
+    pose_vec.push_back(random_pose);
+  }
+  
 
   igndbg<<"final pose vec size "<<pose_vec.size()<<std::endl;
+
   return pose_vec;
 }
 
@@ -145,20 +180,89 @@ void RandomMover::Configure(const gz::sim::Entity &_entity,
   auto model = gz::sim::Model(_entity);
   
   std::string model_name = model.Name(_ecm); 
+  
+  this->data_ptr->model_name = model_name;
 
   std::string move_group; 
 
+  std::vector<GroupedPoseVec>& grouped_pose_vecs = PoseStorage::Instance().get_poses();
+  std::vector<std::string>& move_group_vec = PoseStorage::Instance().get_move_group_set();
+  std::set<std::string> move_group_set(move_group_vec.begin(), move_group_vec.end());
+
   if (_sdf->Get<std::string>("move_group", move_group, "target")){
     ignmsg << "setting " << model_name << " to move group " << move_group <<std::endl;
-    if (move_group == "target"){
-      this->data_ptr->target_move_group.insert(model_name);
-    }
-    else if (move_group == "tracker") {
-      this->data_ptr->tracker_move_group.insert(model_name); 
-    }
+    this->data_ptr->move_group = move_group;
+    GroupedPoseVec move_group_pose_vec;
+    move_group_pose_vec.move_group = move_group;
 
+    if (move_group_set.count(move_group) < 1){
+      grouped_pose_vecs.push_back(move_group_pose_vec);
+      move_group_vec.push_back(move_group);
+      ignmsg<<"made a mode groupe with name: "<<move_group<<std::endl;
+    }
+    
   }
+  ignmsg<<"made move groups: "<<std::endl;
+  for (std::string mvg_name : move_group_vec){
+    ignmsg<<mvg_name<<" ";
+  }
+  ignmsg<<std::endl;
 
+
+
+  // Read numerical parameters like ranges and probabilities
+  double range_min = 7.0, range_max = 20.0; // Default values
+  if (_sdf->HasElement("range"))
+  {
+    std::string range = _sdf->Get<std::string>("range");
+    sscanf(range.c_str(), "%lf %lf", &range_min, &range_max); // Parse range
+    ignmsg<<"setting range from sdf params "<< range_min <<", "<<range_max<<std::endl;
+  }
+  this->data_ptr->range = {range_min, range_max};
+
+  double z_range_min = -0.1, z_range_max = 0.1; // Default values
+  if (_sdf->HasElement("z_range"))
+  {
+    std::string z_range = _sdf->Get<std::string>("z_range");
+    sscanf(z_range.c_str(), "%lf %lf", &z_range_min, &z_range_max); // Parse z_range
+  }
+  this->data_ptr->z_range = {z_range_min, z_range_max};
+
+  double roll_range_min = -0.01, roll_range_max = 0.01; // Default values
+  if (_sdf->HasElement("roll_range"))
+  {
+    std::string roll_range = _sdf->Get<std::string>("roll_range");
+    sscanf(roll_range.c_str(), "%lf %lf", &roll_range_min, &roll_range_max); // Parse roll_range
+  }
+  this->data_ptr->roll_range = {roll_range_min, roll_range_max};
+
+  double pitch_range_min = -0.01, pitch_range_max = 0.01; // Default values
+  if (_sdf->HasElement("pitch_range"))
+  {
+    std::string pitch_range = _sdf->Get<std::string>("pitch_range");
+    sscanf(pitch_range.c_str(), "%lf %lf", &pitch_range_min, &pitch_range_max); // Parse pitch_range
+  }
+  this->data_ptr->pitch_range = {pitch_range_min, pitch_range_max}; 
+
+  double yaw_range_min = 0.0, yaw_range_max = 6.28; // Default values
+  if (_sdf->HasElement("yaw_range"))
+  {
+    std::string yaw_range = _sdf->Get<std::string>("yaw_range");
+    sscanf(yaw_range.c_str(), "%lf %lf", &yaw_range_min, &yaw_range_max); // Parse yaw_range
+  }
+  this->data_ptr->yaw_range = {yaw_range_min, yaw_range_max};
+
+  // Example of extracting additional parameters
+  double appearance_p = 0.5; // Default value
+  if (_sdf->HasElement("appearance_p"))
+    appearance_p = _sdf->Get<double>("appearance_p");
+  this->data_ptr->appearance_p = appearance_p;
+
+
+  double min_dist = 5.0; // Default value
+  if (_sdf->HasElement("min_dist"))
+    min_dist = _sdf->Get<double>("min_dist");
+  this->data_ptr->min_dist = min_dist;
 
   
 
@@ -179,22 +283,9 @@ void RandomMover::Update(const gz::sim::UpdateInfo &_info,
     return;
   }
 
-  auto target_group = this->data_ptr->target_move_group;
-  auto tracker_group = this->data_ptr->tracker_move_group;
-  int n_targets = 0;
+  std::string move_group = this->data_ptr->move_group;
 
-  _ecm.Each<gz::sim::components::Name>(
-    [&](const gz::sim::Entity &_entity, const gz::sim::components::Name *_name) -> bool {
-      // igndbg << gz::sim::Model(_entity).Name(_ecm) << std::endl;
-      if (target_group.count(_name->Data()) > 0){
-        n_targets++;
-      }
-    return true;
-    }
-  );
-
-
-  auto target_poses_vec = generate_random_target_poses(7, 20, 1, 0.5);
+  auto target_poses_vec = generate_random_target_poses();
 
 
   gz::sim::Entity model_entity = gz::sim::kNullEntity; 
@@ -202,24 +293,53 @@ void RandomMover::Update(const gz::sim::UpdateInfo &_info,
   _ecm.Each<gz::sim::components::Name>(
     [&](const gz::sim::Entity &_entity, const gz::sim::components::Name *_name) -> bool {
       // igndbg << gz::sim::Model(_entity).Name(_ecm) << std::endl;
-      if (tracker_group.count(_name->Data()) > 0){
-        igndbg << _name->Data() << std::endl;
-        model_entity = _entity;
-        auto model = gz::sim::Model(model_entity);
-        model.SetWorldPoseCmd(_ecm,generate_random_tracker_pose(0.7, 2, 0.1, 0.1));
+      if (this->data_ptr->model_name == _name->Data()){
+        if (move_group == "tracker"){
+          igndbg << _name->Data() << std::endl;
+          model_entity = _entity;
+          auto model = gz::sim::Model(model_entity);
+          model.SetWorldPoseCmd(_ecm,generate_random_tracker_pose(0.7, 2, 0.1, 0.1));
 
-      }
-      else if (target_group.count(_name->Data()) > 0) {
-        igndbg << _name->Data() << std::endl;
-        model_entity = _entity;
-        auto model = gz::sim::Model(model_entity);
-        
-        auto pose = target_poses_vec.back();
-        target_poses_vec.pop_back();
-
-        model.SetWorldPoseCmd(_ecm, pose);
-
-      
+        }
+        else if (move_group == "target") {
+          igndbg << _name->Data() << std::endl;
+          model_entity = _entity;
+          auto model = gz::sim::Model(model_entity);
+          
+          if (!target_poses_vec.empty()) {
+            auto pose = target_poses_vec.back();
+            target_poses_vec.pop_back();
+            model.SetWorldPoseCmd(_ecm, pose);
+          } else {
+            igndbg << "target_poses_vec is empty; cannot pop back!" << std::endl;
+          }
+        }
+        else if (move_group == "ground") {
+          igndbg << _name->Data() << std::endl;
+          model_entity = _entity;
+          auto model = gz::sim::Model(model_entity);
+          
+          if (!target_poses_vec.empty()) {
+            auto pose = target_poses_vec.back();
+            target_poses_vec.pop_back();
+            model.SetWorldPoseCmd(_ecm, pose);
+          } else {
+            igndbg << "target_poses_vec is empty; cannot pop back!" << std::endl;
+          }
+        }
+        // else {
+        //   igndbg << _name->Data() << std::endl;
+        //   model_entity = _entity;
+        //   auto model = gz::sim::Model(model_entity);
+        //   
+        //   if (!target_poses_vec.empty()) {
+        //     auto pose = target_poses_vec.back();
+        //     target_poses_vec.pop_back();
+        //     model.SetWorldPoseCmd(_ecm, pose);
+        //   } else {
+        //     igndbg << "target_poses_vec is empty; cannot pop back!" << std::endl;
+        //   }
+        // }
       }
       return true;
     }
